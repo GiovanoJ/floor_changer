b7f4596eb906799f840c607bc1eadef9f54355ce
Giovano Julian Somantry <giovano.jsomantry@gmail.com>
2026-04-10 11:01:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -209,10 +209,11 @@ def apply_texture(img_bgr, mask, tex_bgr, tile_size=TEXTURE_TILE_SIZE, feather_r
         r_f = result.astype(np.float32)
         i_f = img.astype(np.float32)
         raw = a3 * r_f + (1.0 - a3) * i_f
-        st.write(f"r_f dtype: {r_f.dtype}")
-        st.write(f"i_f dtype: {i_f.dtype}")  
-        st.write(f"a3 dtype: {a3.dtype}")
-        st.write(f"r_f.flags: {r_f.flags['C_CONTIGUOUS']}")
+        st.write(f"a3 shape: {a3.shape}")
+        st.write(f"r_f shape: {r_f.shape}")
+        st.write(f"i_f shape: {i_f.shape}")
+        st.write(f"(a3 * r_f) di mid: {(a3 * r_f)[ys[mid], xs[mid]]}")
+        st.write(f"((1-a3) * i_f) di mid: {((1.0 - a3) * i_f)[ys[mid], xs[mid]]}"
         result = np.clip(raw, 0, 255).astype(np.uint8)
 
 
